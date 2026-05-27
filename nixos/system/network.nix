a1a996d7cf54005b774227aed33c56f0cf2bae01
Izzi <izzidz@gmail.com>
2026-05-27 17:14:55 +0700

    feat: add spoofdpi

--- nixos/system/network.nix
+++ nixos/system/network.nix
@@ -14,5 +14,25 @@
         denyinterfaces bnep* bt* hci*
       '';
     };
+    proxy = {
+      default = "http://127.0.0.1:8080";
+      noProxy = "127.0.0.1,localhost,*.local,10.0.0.0/8,192.168.0.0/16";
+    };
+  };
+
+  systemd.services.spoofdpi = {
+    description = "Simple and fast anti-censorship proxy tool";
+    after = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
+
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.spoofdpi}/bin/spoofdpi --listen-addr 127.0.0.1:8080 --dns-addr 1.1.1.1:53 --dns-mode https";
+      Restart = "on-failure";
+      RestartSec = "5s";
+
+      ProtectSystem = "full";
+      NoNewPrivileges = true;
+    };
   };
 }
